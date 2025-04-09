d24cce9b1a6578a5393025e281c8cb2e48775743
Samuel-Rundell <runsamuelt@gmail.com>
2025-04-09 14:52:31 -0500

    Basic page setup and working weather API

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__text">Developed by Samuel Rundell </p>
+      <h1 className="footer__year">2025</h1>
+    </footer>
+  );
+}
+
+export default Footer;
