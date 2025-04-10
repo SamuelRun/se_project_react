cbd5c77cadbb315ea16b73ec21202a506f9b1a62
Samuel-Rundell <runsamuelt@gmail.com>
2025-04-09 19:47:06 -0500

    radio button fix/other minor fixes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -50,6 +50,7 @@ function App() {
         buttonText="Add garment"
         activeModal={activeModal}
         onClose={closeActiveModal}
+        isOpen={activeModal === "add-garment"}
       >
         <label htmlFor="name" className="modal__label">
           Name{" "}
@@ -72,19 +73,37 @@ function App() {
         <fieldset className="modal__radio-buttons">
           <legend className="modal__legend">Select the weather type:</legend>
           <label htmlFor="hot" className="modal__label modal__label_type_radio">
-            <input id="hot" type="radio" className="modal__radio-input" /> Hot
+            <input
+              id="hot"
+              type="radio"
+              className="modal__radio-input"
+              name="radio"
+            />{" "}
+            Hot
           </label>
           <label
             htmlFor="warm"
             className="modal__label modal__label_type_radio"
           >
-            <input id="warm" type="radio" className="modal__radio-input" /> Warm
+            <input
+              id="warm"
+              type="radio"
+              className="modal__radio-input"
+              name="radio"
+            />{" "}
+            Warm
           </label>
           <label
             htmlFor="cold"
             className="modal__label modal__label_type_radio"
           >
-            <input id="cold" type="radio" className="modal__radio-input" /> Cold
+            <input
+              id="cold"
+              type="radio"
+              className="modal__radio-input"
+              name="radio"
+            />{" "}
+            Cold
           </label>
         </fieldset>
       </ModalWithForm>
